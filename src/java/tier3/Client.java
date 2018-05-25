9022d5374d38b656a2bc5944005fc1b93ef29697
duchesnj <julien.d.duchesne@gmail.com>
2018-05-25 10:23:39 +0200

    ajout client rmi

---
+++ src/java/tier3/Client.java
@@ -0,0 +1,16 @@
+package tier3;
+
+import java.rmi.Naming;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
+
+public class Client
+{
+    public static void main(String [] args) throws Exception
+    {
+        System.setProperty("java.rmi.server.hostname", "86.76.4.24");        
+        TwitterDBService service = (TwitterDBService) Naming.lookup("rmi://86.76.4.24:3200/TwitterDBService");
+
+        System.out.println("Nous sommes connectés");
+    }
+}
